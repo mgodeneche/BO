17d3ae40a8bfa9a9690d91d7f4670b76e0fc4e82
maximemeri <maximemeri@gmail.com>
2014-06-16 15:31:14 +0200

    Finalisation Service
    ajout des derniers services de l'application

---
+++ src/main/java/com/epsi/tpecommerce/service/HistoStatutCmdService.java
@@ -0,0 +1,17 @@
+package com.epsi.tpecommerce.service;
+
+import com.epsi.tpecommerce.dao.HistoStatutCmdDao;
+import com.epsi.tpecommerce.entity.HistoriqueStatutCmd;
+
+public class HistoStatutCmdService {
+	
+	HistoStatutCmdDao histoStatutCmdDao;
+	
+	public HistoStatutCmdService() {
+		histoStatutCmdDao = new HistoStatutCmdDao();
+	}
+
+	public HistoriqueStatutCmd find(int id) {
+		return histoStatutCmdDao.find(id);
+	}
+}
---
+++ src/main/java/com/epsi/tpecommerce/service/StatutCmdService.java
@@ -0,0 +1,17 @@
+package com.epsi.tpecommerce.service;
+
+import com.epsi.tpecommerce.dao.StatutCmdDao;
+import com.epsi.tpecommerce.entity.StatutCmd;
+
+public class StatutCmdService {
+	
+	StatutCmdDao statutCmdDao;
+	
+	public StatutCmdService() {
+		statutCmdDao = new StatutCmdDao();
+	}
+
+	public StatutCmd find(int id) {
+		return statutCmdDao.find(id);
+	}
+}
